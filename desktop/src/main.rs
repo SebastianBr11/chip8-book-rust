4b165036ac6e9bcf28d79260e76b5161a277b8c7
Sebastian Breit <sebastian.breitan+github@gmail.com>
2022-07-21 05:40:00 +0000

    Draw game to screen

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,11 +1,11 @@
 use chip8_core::*;
-use sdl2::event::Event;
-use std::env;
+use sdl2::{event::Event, pixels::Color, rect::Rect, render::Canvas, video::Window};
+use std::{env, fs::File, io::Read};
 
 const SCALE: u32 = 15;
 const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
 const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
-
+const TICKS_PER_FRAME: usize = 10;
 fn main() {
     let args: Vec<_> = env::args().collect();
     if args.len() != 2 {
@@ -29,6 +29,13 @@ fn main() {
 
     let mut event_pump = sdl_context.event_pump().unwrap();
 
+    let mut chip8 = Emu::new();
+
+    let mut rom = File::open(&args[1]).expect("Unable to open file");
+    let mut buffer = Vec::new();
+    rom.read_to_end(&mut buffer).unwrap();
+    chip8.load(&buffer);
+
     'gameloop: loop {
         for evt in event_pump.poll_iter() {
             match evt {
@@ -37,6 +44,33 @@ fn main() {
                 }
                 _ => (),
             }
+        }
+
+        for _ in 0..TICKS_PER_FRAME {
+            chip8.tick();
+        }
+        chip8.tick_timers();
+        draw_screen(&chip8, &mut canvas);
+    }
+}
+
+fn draw_screen(emu: &Emu, canvas: &mut Canvas<Window>) {
+    // Clear canvas as black
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+
+    let screen_buf = emu.get_display();
+    // Now set draw color to white, iterate through each point and see if it should be drawn
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+    for (i, pixel) in screen_buf.iter().enumerate() {
+        if *pixel {
+            // Convert our 1D array's index into a 2D (x, y) position
+            let x = (i % SCREEN_WIDTH) as u32;
+            let y = (i / SCREEN_WIDTH) as u32;
+
+            // Draw a rectangle at (x, y), scaled up by our SCALE value
+            let rect = Rect::new((x * SCALE) as i32, (y * SCALE) as i32, SCALE, SCALE);
+            canvas.fill_rect(rect).unwrap();
         }
     }
 }
